ed8366841f67d42dcbe56ae069479005d98221d5
xcajkaj <xcajkaj@stuba.sk>
2022-12-29 03:46:35 +0100

    main menu final version + dopredu prepojenie k logike

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-function openModal(){
-    document.getElementById("modal").style.visibility = "visible";
+function goBack(){
+    document.getElementById("modalDifficulty").style.visibility = "hidden";
+    document.getElementById("modalRules").style.visibility = "hidden";
+    document.getElementById("modalAuthors").style.visibility = "hidden";
+    document.getElementById("menu").style.visibility = "visible";
+}
+
+function openModalDifficulty(){
+    document.getElementById("modalDifficulty").style.visibility = "visible";
+    document.getElementById("menu").style.visibility = "hidden";
+}
+
+function openModalRules(){
+    document.getElementById("modalRules").style.visibility = "visible";
+    document.getElementById("menu").style.visibility = "hidden";
+}
+
+function openModalAuthors(){
+    document.getElementById("modalAuthors").style.visibility = "visible";
+    document.getElementById("menu").style.visibility = "hidden";
+}
+
+function startGameEasy(){
+    document.getElementById("modalDifficulty").style.visibility = "hidden";
+}
+
+function startGameHard(){
+    document.getElementById("modalDifficulty").style.visibility = "hidden";
+    document.getElementById("modalGame").innerHTML = `
+    <div>
+        tu bude otazka
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="element">
+                tu bude krajina co treba uhadnut
+            </div>
+            <div class="element">
+                tu bude counter na chyby
+            </div>
+        </div>
+        <div class="column">
+            tu bude drop container
+        </div>
+    </div>
+    <div>
+        tu budu vlajky na dragovanie
+    </div>
+    `
+    document.getElementById("modalGame").style.visibility = "visible";
+
 }
