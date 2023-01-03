267494dbbf80b99991d50c622433df9d1ddd8218
matkoT123 <121394439+matkoT123@users.noreply.github.com>
2023-01-03 14:58:59 +0100

    Add files via upload
    FLAG DROP FINAL VERSION

--- FlagDrop-main/script.js
+++ FlagDrop-main/script.js
@@ -1,4 +1,11 @@
+let countOfroundEasy = 0;
+let canGoNext = true;
+let mistakesEasy = 0;
+
 function goBack(){
+  countOfroundEasy = 0;
+  canGoNext = true;
+  mistakesEasy = 0;
     document.getElementById("modalGame").style.visibility = "hidden";
     document.getElementById("modalDifficulty").style.visibility = "hidden";
     document.getElementById("modalRules").style.visibility = "hidden";
@@ -7,6 +14,7 @@ function goBack(){
 }
 
 function openModalDifficulty(){
+    countOfroundEasy = 0;
     document.getElementById("modalDifficulty").style.visibility = "visible";
     document.getElementById("menu").style.visibility = "hidden";
 }
@@ -21,10 +29,17 @@ function openModalAuthors(){
     document.getElementById("menu").style.visibility = "hidden";
 }
 
-function startGameEasy(){
+
+
+let flagTaken = [];
+
+
+  function startGameEasy(){
+    if(canGoNext) {
     document.getElementById("modalDifficulty").style.visibility = "hidden";
 
-    document.getElementById("modalGame").innerHTML = `
+    if(countOfroundEasy < 5) {
+      document.getElementById("modalGame").innerHTML = `
     <div class="content">
     
     <h4 id="question"></h4>
@@ -43,7 +58,7 @@ function startGameEasy(){
 
     <section id="scoreContainter" class="scoreContainterClass">
       <div id="scoreBox" class="scoreBoxClass">
-        MISTAKES: <span id="score" class="scoreClass">0</span>
+        mistakes  : <span id="score" class="scoreClass">0</span>
       </div>
     </section>
     <section id="controlButtons" class="controlButtons">
@@ -52,180 +67,300 @@ function startGameEasy(){
                 <button class="button-35" type="button" onclick="goBack()">Go Back</button>
             </div>
             <div class="buttonDiv">
-                <button class="button-35" type="button" onclick="">Next</button>
+                <button class="button-35" type="button" onclick="startGameEasy()">Next</button>
             </div>
         </div>
     </section>
   </div>
     `;
-    $.ajax({
-        type: "GET",
-        url: "data.json",
-        dataType: "json",
 
-        success: function (data) {
-            document.getElementById("question").innerHTML = data.questionFlags;
-        }
-    });
-    gameLogic();
+    gameLogic('easy');
     document.getElementById("modalGame").style.visibility = "visible";
+    } else {
+      document.getElementById("modalGame").innerHTML = `
+    <div class="content">
     
+    <h4 id="wholeMistakes"></h4>
+
+    <section id="controlButtons" class="controlButtons">
+        <div class="row">
+            <div class="buttonDiv">
+                <button class="button-35" type="button" onclick="goBack()">Menu</button>
+            </div>
+            <div class="buttonDiv"> 
+                <button class="button-35" type="button" onclick="startGameEasy()">One more</button>
+            </div>
+        </div>
+    </section>
+    
+    
+  </div>
+    `;
+    document.getElementById("wholeMistakes").innerHTML = "Mistakes in this game: " + mistakesEasy;
+    countOfroundEasy = 0;
+    mistakesEasy = 0;
+    }
+
+  }
 
 }
+
+let mistakesHard = 0;
 
 function startGameHard(){
+  if(countOfroundEasy < 5) {
     document.getElementById("modalDifficulty").style.visibility = "hidden";
-    document.getElementById("modalGame").innerHTML = `
-    <div>
-        tu bude otazka
+  document.getElementById("modalGame").innerHTML = `
+  <div class="content">
+  
+  <h4 id="question"></h4>
+  
+  <section class="nameOfStatesContainer">
+    <div id="argentinaDrop" class="nameOfStates" data-draggable-id="argentina">
+      <span id="span">Argentína</span>
     </div>
-    <div class="row">
-        <div class="column">
-            <div>
-                tu bude krajina co treba uhadnut
-            </div>
-            <div>
-                tu bude counter na chyby
-            </div>
-        </div>
-        <div>
-            tu bude drop container
-        </div>
+  </section>
+  
+  <section class="flag-container" id="flagContainer">
+    <img draggable="true" class="flags" src="India.png" id="india" alt="India">
+    <img draggable="true" class="flags" src="Argentina.png" id="argentina" alt="Argentina">
+    <img draggable="true" class="flags" src="Britania.png" id="britania" alt="Britania">
+  </section>
+
+  <section id="scoreContainter" class="scoreContainterClass">
+    <div id="scoreBox" class="scoreBoxClass">
+      mistakes: <span id="score" class="scoreClass">0</span>
     </div>
-    <div>
-        tu budu vlajky na dragovanie
-    </div>
-    `;
-    
-    document.getElementById("modalGame").style.visibility = "visible";
-    
-
-    
-}
-
-
-function gameLogic() {
-    console.log('moore');
-
-// gameLogic
-let images = document.querySelectorAll(".flags");
-let dropFlagTo = document.getElementById("argentinaDrop");
-let nameOfState = document.getElementById("span");
-let mistakesSpan = document.getElementById("score");
-let dragItem = null;
-let idOfFlag = null;
-let isInDropArea = false;
-let mistakes = 0;
-
-//Touch
-for (var j of images) {
-
-  j.addEventListener("touchmove", onTouchMove);
-  j.addEventListener("touchend", onTouchEnd);
-
-  function onTouchMove(e) {
-    e.preventDefault();
-    j = this;
-    j.style.opacity = 1;
-    let touchLocation = e.targetTouches[0];
-    j.style.position = "absolute";
-    leftPos = j.style.left;
-    topPos = j.style.top;
-    j.style.left = (touchLocation.pageX - (j.width / 2)) + 'px';
-    j.style.top = (touchLocation.pageY - (j.height / 2)) + 'px';
-    idOfFlag = this.id;
-
-    let dropFlagToPosition = dropFlagTo.getBoundingClientRect();
-
-    isInDropArea = (
-      (touchLocation.pageX) > dropFlagToPosition.left &&
-      (touchLocation.pageX) < (dropFlagToPosition.left + j.width) &&
-      (touchLocation.pageY) > dropFlagToPosition.top &&
-      (touchLocation.pageY) < (dropFlagToPosition.top + j.height)
-    );
-
-    if (isInDropArea) {
-      dropFlagTo.style.border = "5px dashed black";
-
-    } else if (!isInDropArea) {
-      dropFlagTo.style.border = "3px dashed #111";
-    }
-
-  }
-
-  function onTouchEnd(e) {
-    e.preventDefault;
-    if (dropFlagTo.getAttribute("data-draggable-id") === idOfFlag) {
-      if (isInDropArea) {
-        dropFlagTo.style.border = "3px solid #111";
-        dropFlagTo.append(j);
-        nameOfState.style.display = "none";
-      }
-    } else {
-      j.style.opacity = 0.8;
-      mistakes += 1;
-      mistakesSpan.innerHTML = mistakes;
-      dropFlagTo.style.border = "3px dashed #111";
-      j.style.left = leftPos;
-      j.style.top = topPos;
-    }
-    j.style.position = "unset";
-    idOfFlag = null;
-    isInDropArea = false;
-  }
-}
-
-//Mouse
-for (var i of images) {
-
-  i.addEventListener("dragstart", dragStart);
-  i.addEventListener("dragend", dragEnd);
-
-  function dragStart() {
-    console.log(this);
-    dragItem = this;
-    setTimeout(() => this.style.display = "none", 0);
-    idOfFlag = this.id;
-  }
-
-  function dragEnd() {
-    setTimeout(() => this.style.display = "block", 0);
-    dragItem = null;
-    idOfFlag = null;
-  }
-}
-
-dropFlagTo.addEventListener('dragover', dragOver);
-dropFlagTo.addEventListener('dragenter', dragEnter);
-dropFlagTo.addEventListener('dragleave', dragLeave);
-dropFlagTo.addEventListener('drop', Drop);
-
-function Drop() {
-  this.style.border = "3px dashed #111";
-  if (this.getAttribute("data-draggable-id") === idOfFlag) {
-    this.style.border = "3px solid #111";
-    console.log(this.getAttribute("data-draggable-id"));
-    this.append(dragItem);
-    dragItem.style.opacity = 1;
-    nameOfState.style.display = "none";
+  </section>
+  <section id="controlButtons" class="controlButtons">
+      <div class="row">
+          <div class="buttonDiv">
+              <button class="button-35" type="button" onclick="goBack()">Go Back</button>
+          </div>
+          <div class="buttonDiv">
+              <button class="button-35" type="button" onclick="startGameHard()">Next</button>
+          </div>
+      </div>
+  </section>
+</div>
+  `;
+  gameLogic('hard');
+  document.getElementById("modalGame").style.visibility = "visible";
   } else {
-    mistakes += 1;
-    mistakesSpan.innerHTML = mistakes;
+    document.getElementById("modalGame").innerHTML = `
+    <div class="content">
+    
+    <h4 id="wholeMistakes"></h4>
+
+    <section id="controlButtons" class="controlButtons">
+        <div class="row">
+            <div class="buttonDiv">
+                <button class="button-35" type="button" onclick="goBack()">Menu</button>
+            </div>
+            <div class="buttonDiv"> 
+                <button class="button-35" type="button" onclick="startGameHard()">One more</button>
+            </div>
+        </div>
+    </section>
+    
+    
+  </div>
+    `;
+    document.getElementById("wholeMistakes").innerHTML = "Mistakes in this game: " + mistakesEasy;
+    countOfroundEasy = 0;
+    mistakesEasy = 0;
   }
+  
 }
 
-function dragOver(e) {
-  e.preventDefault();
-  this.style.border = "5px dashed black";
-}
+function gameLogic(difficulty) {
+  canGoNext = false;
+  let images = document.querySelectorAll(".flags");
+  let dropFlagTo = document.getElementById("argentinaDrop");
+  let nameOfState = document.getElementById("span");
+  let mistakesEasySpan = document.getElementById("score");
+  let dragItem = null;
+  let idOfFlag = null;
+  let isInDropArea = false;
+  let arrayGenerate = [];
+  let arrayImages = [];
 
-function dragEnter(e) {
-  e.preventDefault();
-  this.style.border = "3px dashed #111";
-}
+  mistakesEasySpan.innerHTML = mistakesEasy;
+  
+  fetch('data.json').then(response => {
+    return response.json();
+  }).then(data => {
 
-function dragLeave() {
-  this.style.border = "3px dashed #111";
-}
+    if(difficulty == "easy") {
+      document.getElementById("question").innerHTML = data.questionFlags;
+  
+    for (var k of images) {
+      a = Math.floor((Math.random() * data.flags.length));
+      
+      if (!arrayGenerate.includes(a)) {
+        arrayGenerate.push(a);
+      } else {
+        a = Math.floor((Math.random() * data.flags.length));
+        if (arrayGenerate.includes(a)) {
+          a = Math.floor((Math.random() * data.flags.length));
+          arrayGenerate.push(a);
+        } else {
+          arrayGenerate.push(a);
+        }
+      }
+      k.setAttribute("alt", data.flags[a].title);
+      k.setAttribute("src", data.flags[a].path);
+      k.setAttribute("id", data.flags[a].id);
+  
+      arrayImages.push(k);
+      flagTaken.push(k);
+
+    }
+    }
+
+    if(difficulty == "hard") {
+      document.getElementById("question").innerHTML = data.questionShapes;
+  
+    for (var k of images) {
+      a = Math.floor((Math.random() * data.flags.length));
+      
+      if (!arrayGenerate.includes(a)) {
+        arrayGenerate.push(a);
+      } else {
+        a = Math.floor((Math.random() * data.flags.length));
+        if (arrayGenerate.includes(a)) {
+          a = Math.floor((Math.random() * data.flags.length));
+          arrayGenerate.push(a);
+        } else {
+          arrayGenerate.push(a);
+        }
+      }
+      k.setAttribute("alt", data.shapes[a].title);
+      k.setAttribute("src", data.shapes[a].path);
+      k.setAttribute("id", data.shapes[a].id);
+  
+      arrayImages.push(k);
+      flagTaken.push(k);
+
+    }
+    }
+
+  let generateIndex = Math.floor((Math.random() * 3));;
+  dropFlagTo.setAttribute("data-draggable-id", arrayImages[generateIndex].id);
+  nameOfState.innerHTML = arrayImages[generateIndex].alt;
+  
+  //Touch
+  for (var j of images) {
+    j.addEventListener("touchmove", onTouchMove);
+    j.addEventListener("touchend", onTouchEnd);
+  
+    function onTouchMove(e) {
+      e.preventDefault();
+      j = this;
+      j.style.opacity = 1;
+      let touchLocation = e.targetTouches[0];
+      j.style.position = "absolute";
+      leftPos = j.style.left;
+      topPos = j.style.top;
+      j.style.left = (touchLocation.pageX - (j.width / 2)) + 'px';
+      j.style.top = (touchLocation.pageY - (j.height / 2)) + 'px';
+      idOfFlag = this.id;
+  
+      let dropFlagToPosition = dropFlagTo.getBoundingClientRect();
+  
+      isInDropArea = (
+        (touchLocation.pageX) > dropFlagToPosition.left &&
+        (touchLocation.pageX) < (dropFlagToPosition.left + j.width) &&
+        (touchLocation.pageY) > dropFlagToPosition.top &&
+        (touchLocation.pageY) < (dropFlagToPosition.top + j.height)
+      );
+  
+      if (isInDropArea) {
+        dropFlagTo.style.border = "5px dashed black";
+  
+      } else if (!isInDropArea) {
+        dropFlagTo.style.border = "3px dashed #111";
+      }
+  
+    }
+  
+    function onTouchEnd(e) {
+      e.preventDefault;
+      if (dropFlagTo.getAttribute("data-draggable-id") === idOfFlag) {
+        if (isInDropArea) {
+          dropFlagTo.style.border = "3px solid #111";
+          dropFlagTo.append(j);
+          nameOfState.style.display = "none";
+          countOfroundEasy++;
+          canGoNext = true;
+        }
+      } else {
+        j.style.opacity = 0.8;
+        mistakesEasy += 1;
+        mistakesEasySpan.innerHTML = mistakesEasy;
+        dropFlagTo.style.border = "3px dashed #111";
+        j.style.left = leftPos;
+        j.style.top = topPos;
+      }
+      j.style.position = "unset";
+      idOfFlag = null;
+      isInDropArea = false;
+    }
+  }
+  
+  //Mouse
+  for (var i of images) {
+  
+    i.addEventListener("dragstart", dragStart);
+    i.addEventListener("dragend", dragEnd);
+  
+    function dragStart() {
+      dragItem = this;
+      setTimeout(() => this.style.display = "none", 0);
+      idOfFlag = this.id;
+    }
+  
+    function dragEnd() {
+      setTimeout(() => this.style.display = "block", 0);
+      dragItem = null;
+      idOfFlag = null;
+    }
+  }
+  
+  dropFlagTo.addEventListener('dragover', dragOver);
+  dropFlagTo.addEventListener('dragenter', dragEnter);
+  dropFlagTo.addEventListener('dragleave', dragLeave);
+  dropFlagTo.addEventListener('drop', Drop);
+  
+  function Drop() {
+    this.style.border = "3px dashed #111";
+    if (this.getAttribute("data-draggable-id") === idOfFlag) {
+      this.style.border = "3px solid #111";
+      countOfroundEasy++;
+      canGoNext = true;
+      this.append(dragItem);
+      dragItem.style.opacity = 1;
+      nameOfState.style.display = "none";
+    } else {
+      mistakesEasy += 1;
+      mistakesEasySpan.innerHTML = mistakesEasy;
+    }
+  }
+  
+  function dragOver(e) {
+    e.preventDefault();
+    this.style.border = "5px dashed black";
+  }
+  
+  function dragEnter(e) {
+    e.preventDefault();
+    this.style.border = "3px dashed #111";
+  }
+  
+  function dragLeave() {
+    this.style.border = "3px dashed #111";
+  }
+  
+  })
 
 }
